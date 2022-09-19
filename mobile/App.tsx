190176c1d9e0b796f822941084b63f8a1140eb19
devSirNaelC7 <sirnaelc7@gmail.com>
2022-09-18 21:24:17 -0300

    aula-04

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -8,7 +8,7 @@ import {
   Inter_900Black
 } from '@expo-google-fonts/inter';
 
-import {Home} from './src/screens/Home';
+import { Routes } from './src/routes';
 import { Background } from './src/components/Background';
 import { Loading } from './src/components/Loading';
 
@@ -24,13 +24,13 @@ export default function App() {
 
   return (
     <Background>
-      <StatusBar 
-      barStyle='light-content'
-      backgroundColor='transparent'
-      translucent
-      
+      <StatusBar
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+
       />
-      {fontsLoaded ? <Home /> : <Loading/>}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </Background>
 
   );
